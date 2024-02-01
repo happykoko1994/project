c06bbcdb3a6f3e3d666cf00e7ee44c6dc6884e82
Maksim <max-goryachev@ya.ru>
2024-02-01 20:19:07 +0600

    qwe

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const button = document.querySelector('.play');
-const roullete = document.querySelector('.roullete')
-const finish = document.querySelector('.finish')
-button.addEventListener('click',()=>{
-    roullete.style.display = 'flex'
-    setTimeout(() => {
-        finish.style.display = 'flex'
-    }, 2000);
+document.addEventListener('DOMContentLoaded',()=>{
+    const button = document.querySelector('.play');
+    const roullete = document.querySelector('.roullete')
+    const finish = document.querySelector('.finish')
+    button.addEventListener('click',()=>{
+        roullete.style.display = 'flex'
+        setTimeout(() => {
+            finish.style.display = 'flex'
+        }, 2000);
+    })
 })
