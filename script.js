8deb442656ba61413ae17b8c8749a8c6a5b4e1d8
Maksim <max-goryachev@ya.ru>
2024-02-02 17:57:08 +0600

    qwe

--- script.js
+++ script.js
@@ -7,14 +7,34 @@ const rightText = document.querySelector('.right-text')
     const bgmain = document.querySelector('.bgmain')
     const factBg = document.querySelector('.fact-bg')
     const play = document.querySelector('.play')
+    const prize = document.querySelector('.prize')
+    
+
     button.addEventListener('click',()=>{
         rightText.style.display = 'none'
         factBg.style.display = 'none'
         play.style.display = 'none'
         bgmain.style.display = 'none'
         roullete.style.display = 'flex'
-        setTimeout(() => {
-            finish.style.display = 'flex'
-        }, 2000);
+
+        var cards_block = document.querySelector('.cards');
+        var cards = document.querySelectorAll('.cards > .card');
+
+        var random = Math.floor(Math.random() * (10 - 0) + 0)
+        
+        console.log(cards[random],random)
+        setTimeout(function() {
+
+            cards_block.style.left =Math.floor(Math.random() * (350 - 250) + 250)-random * 158 + 'px';
+            setTimeout(() => {
+                prize.append(cards[random])
+                roullete.style.display = 'none'
+                finish.style.display = 'flex'
+              }, 6000);
+        
+
+          }, 500)
+
+          
     })
 })
