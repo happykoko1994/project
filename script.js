bcd23f7df772862ea6e3ed16a0b2aa446a35c5ad
Maksim <max-goryachev@ya.ru>
2024-02-02 15:35:49 +0600

    qwe

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+
 document.addEventListener('DOMContentLoaded',()=>{
+const rightText = document.querySelector('.right-text')
     const button = document.querySelector('.play');
     const roullete = document.querySelector('.roullete')
     const finish = document.querySelector('.finish')
+    const bgmain = document.querySelector('.bgmain')
+    const factBg = document.querySelector('.fact-bg')
+    const play = document.querySelector('.play')
     button.addEventListener('click',()=>{
+        rightText.style.display = 'none'
+        factBg.style.display = 'none'
+        play.style.display = 'none'
+        bgmain.style.display = 'none'
         roullete.style.display = 'flex'
         setTimeout(() => {
             finish.style.display = 'flex'
