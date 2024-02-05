4bf96ffbc539c05195cbaa29a5f58509ec1eb231
Maksim <max-goryachev@ya.ru>
2024-02-05 16:01:44 +0600

    qew

--- script.js
+++ script.js
@@ -8,77 +8,81 @@ const rightText = document.querySelector('.right-text')
     const factBg = document.querySelector('.fact-bg')
     const play = document.querySelector('.play')
     const prize = document.querySelector('.prize')
-
-    if(window.screen.availWidth>768){
-        button.addEventListener('click',()=>{
-            rightText.style.display = 'none'
-            factBg.style.display = 'none'
-            play.style.display = 'none'
-            bgmain.style.display = 'none'
-            roullete.style.display = 'flex'
-            let cards_block = document.querySelector('.cards');
-            let cards = document.querySelectorAll('.cards > .card'); 
-
-            for (let index = 0; index < cards.length; index++) {
-                let new_element = cards[index].cloneNode(true);
-                cards_block.append(new_element); 
+    if (localStorage.getItem("rolls") === null){
+        if(window.screen.availWidth>768){
+            button.addEventListener('click',()=>{
+                rightText.style.display = 'none'
+                factBg.style.display = 'none'
+                play.style.display = 'none'
+                bgmain.style.display = 'none'
+                roullete.style.display = 'flex'
+                let cards_block = document.querySelector('.cards');
+                let cards = document.querySelectorAll('.cards > .card'); 
+    
+                for (let index = 0; index < cards.length; index++) {
+                    let new_element = cards[index].cloneNode(true);
+                    cards_block.append(new_element); 
+                    
+                }
+    
+                let random = Math.floor(Math.random() * (cards_block.children.length - (cards_block.children.length/2+1)) + (cards_block.children.length/2+1))
                 
-            }
-            console.log(cards_block.children.length)
-
-            let random = Math.floor(Math.random() * (cards_block.children.length - 11) + 11)
-            
-            console.log(cards_block.children[random],random)
-            setTimeout(function() {
-    
-                cards_block.style.left =Math.floor(Math.random() * (350 - 250) + 250)-random * 158 + 'px';
-                setTimeout(() => {
-                    prize.append(cards_block.children[random])
-                    roullete.style.display = 'none'
-                    finish.style.display = 'flex'
-                  }, 6000);
-            
-    
-              }, 500)
-    
-              
-        })
-    }else{
-        console.log('mobile');
-        button.addEventListener('click',()=>{
-            rightText.style.display = 'none'
-            factBg.style.display = 'none'
-            play.style.display = 'none'
-            bgmain.style.display = 'none'
-            roullete.style.display = 'flex'
-            let cards_block = document.querySelector('.cards');
-            let cards = document.querySelectorAll('.cards > .card'); 
-
-            for (let index = 0; index < cards.length; index++) {
-                let new_element = cards[index].cloneNode(true);
-                cards_block.append(new_element); 
-                
-            }
-            console.log(cards_block.children.length)
-
-            let random = Math.floor(Math.random() * (cards_block.children.length - 11) + 11)
-            
-            console.log(cards_block.children[random],random)
-            setTimeout(function() {
-    
-                cards_block.style.top =Math.floor(Math.random() * (290 - 190) + 190)-random * 192 + 'px';
-                setTimeout(() => {
-                    prize.append(cards_block.children[random])
-                    roullete.style.display = 'none'
-                    finish.style.display = 'flex'
-                  }, 6000);
-            
-    
-              }, 500)
-    
-              
-        })
+                console.log(cards_block.children[random],random)
+                setTimeout(function() {
         
-    }
+                    cards_block.style.left =Math.floor(Math.random() * (350 - 250) + 250)-random * 158 + 'px';
+                    setTimeout(() => {
+                        prize.append(cards_block.children[random])
+                        roullete.style.display = 'none'
+                        finish.style.display = 'flex'
+                      }, 6000);
+                
+        
+                  }, 500)
+        
+                  localStorage.setItem('rolls', true);
+            })
+        }else{
+            console.log('mobile');
+            button.addEventListener('click',()=>{
+                rightText.style.display = 'none'
+                factBg.style.display = 'none'
+                play.style.display = 'none'
+                bgmain.style.display = 'none'
+                roullete.style.display = 'flex'
+                let cards_block = document.querySelector('.cards');
+                let cards = document.querySelectorAll('.cards > .card'); 
+    
+                for (let index = 0; index < cards.length; index++) {
+                    let new_element = cards[index].cloneNode(true);
+                    cards_block.append(new_element); 
+                    
+                }
+    
+                let random = Math.floor(Math.random() * (cards_block.children.length - (cards_block.children.length/2+1)) + (cards_block.children.length/2+1))
+                
+                console.log(cards_block.children[random],random)
+                setTimeout(function() {
+        
+                    cards_block.style.top =Math.floor(Math.random() * (290 - 190) + 190)-random * 192 + 'px';
+                    setTimeout(() => {
+                        prize.append(cards_block.children[random])
+                        roullete.style.display = 'none'
+                        finish.style.display = 'flex'
+                      }, 6000);
+                
+        
+                  }, 500)
+        
+                  
+            })
+            localStorage.setItem('rolls', true);
 
-})
+        }
+    }else{
+        alert('Ты уже крутил!');
+        localStorage.clear();
+    }
+    })
+    
+    
